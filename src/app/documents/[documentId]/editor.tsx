d397575d0dc75c7938b586748052a0d015f73cc1
rohinish404 <singhrohinish09@gmail.com>
2025-07-17 11:40:43 +0530

    add ruler

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -22,10 +22,13 @@ import { LineHeightExtension } from "@/extensions/line-height";
 import { useEditorStore } from "@/store/use-editor-store";
 import { FontSizeExtension } from "@/extensions/font-size";
 
+import { Ruler } from "./ruler";
+
 export const Editor = () => {
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor);
     },
@@ -103,6 +106,7 @@ export const Editor = () => {
   });
   return (
     <div className="size-full overflow-x-auto bg-[#F9FAFD] px-4 print:p-0 print:bg-white print:pverflow-visible">
+      <Ruler />
       <div className="min-w-max justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />;
       </div>
